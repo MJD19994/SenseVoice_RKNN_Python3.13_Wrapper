645e33be70bf947dde7a5758d5a12235dcad56ab
MJD19994 <mttdudley@gmail.com>
2026-02-13 23:30:13 -0500

    Create lib/utils/frontend.py file

---
+++ lib/utils/frontend.py
@@ -0,0 +1,7 @@
+import sys
+from pathlib import Path
+MODEL_PATH = Path("/root/sensevoice-server/models/models--happyme531--SenseVoiceSmall-RKNN2/snapshots/2b134bc175c5bc16ec315613d183eb34b0748043")
+if str(MODEL_PATH) not in sys.path:
+    sys.path.insert(0, str(MODEL_PATH))
+from sensevoice_rknn import WavFrontend
+__all__ = ['WavFrontend']
